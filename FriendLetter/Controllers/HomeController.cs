23c4cc152696f85801c41ffb6d164441bb5ce6fe
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-18 09:27:09 -0700

    change paths to navigate to form on page load, add Razor code to letter form to output proper input

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -11,15 +11,15 @@ namespace FriendLetter.Controllers
     [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
 
+    // [Route("/")]
+    // public ActionResult Letter()
+    // {
+    //   LetterVariable myLetterVariable = new LetterVariable();
+    //   myLetterVariable.Recipient = "Lina";
+    //   myLetterVariable.Sender = "Jasmine";
+    //   return View(myLetterVariable);
+    // }
     [Route("/")]
-    public ActionResult Letter()
-    {
-      LetterVariable myLetterVariable = new LetterVariable();
-      myLetterVariable.Recipient = "Lina";
-      myLetterVariable.Sender = "Jasmine";
-      return View(myLetterVariable);
-    }
-    [Route("/form")]
     public ActionResult Form() { return View(); }
     [Route("/postcard")]
     public ActionResult Postcard(string recipient, string sender, string location, string souvenir)
